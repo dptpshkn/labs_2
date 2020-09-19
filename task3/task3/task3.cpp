be0167b21ddbdb38b47512da624ead013c750783
danilapotapeshkin@outlook.com <danilapotapeshkin@outlook.com>
2020-09-19 18:33:21 +0300

    Это всё 4 лабораторная

---
+++ task3/task3/task3.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a, b, sr;
+	cin >> a;
+	cin >> b;
+	sr = (a + b) / 2;
+	cout << "Srednee arifmeticheskoe ravno: " << sr;
+	return 0;
+}
