1161422472d8bdf7c58f1763ebada8eb185c8d72
danilapotapeshkin@outlook.com <danilapotapeshkin@outlook.com>
2020-09-19 19:06:43 +0300

    Последнее задание 4 лабораторной.

---
+++ task5/task5/task5.cpp
@@ -0,0 +1,16 @@
+﻿/*Даны два ненулевых числа. Найти сумму, разность, произведение и
+частное их модулей.*/
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a;
+    cin >> b;
+    cout << abs(a) + abs(b) << '\n';
+    cout << abs(a) - abs(b) << '\n';
+    cout << abs(a) * abs(b) << '\n';
+    cout << abs(a) / abs(b) << '\n';
+    return 0;
+}
+
