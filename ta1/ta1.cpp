7e8536a1c1c47ac3039dde7b59f4d394691d5772
danilapotapeshkin@outlook.com <danilapotapeshkin@outlook.com>
2020-09-19 18:06:52 +0300

    Add project files.

---
+++ ta1/ta1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a, b, s, p;
+	cin >> a;
+	cin >> b;
+	s = a * b;
+	p = (a + b) * 2;
+	cout << "Ploshad'ravna: " << s << '\n';
+	cout << "Perimetr raven: " << p << '\n';
+	return 0;
+}
