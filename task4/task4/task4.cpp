53f217064a63780eda29d6011a5ab19c94645a18
danilapotapeshkin@outlook.com <danilapotapeshkin@outlook.com>
2020-09-19 18:48:57 +0300

    .

---
+++ task4/task4/task4.cpp
@@ -0,0 +1,14 @@
+﻿/*Даны два ненулевых числа. Найти сумму, разность, произведение и
+частное их квадратов.*/
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b;
+	cin >> a;
+	cin >> b;
+	cout << a * a + b * b << '\n';
+	cout << a * a - b * b << '\n';
+	cout << a * a * b * b << '\n';
+	cout << a * a / b * b << '\n';
+	return 0;
+}
